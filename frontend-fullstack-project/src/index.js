fea15120a3f9c362ecb2f277c4612299443769d2
Varlyte <lucas06330@gmail.com>
2022-03-20 13:59:53 +0100

    skeletton

--- frontend-fullstack-project/src/index.js
+++ frontend-fullstack-project/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
+import Home from './routes/Home';
+import InputLog from './routes/inputLog.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>} >
+          <Route index element={<InputLog/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="*" element={<main><p>There is nothing here!</p></main>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
